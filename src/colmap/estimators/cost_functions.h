7f474c82803939befa640ca000fc1c9a2fc9684d
daszhugh@gmail.com <daszhugh>
2024-02-02 20:52:19 +0800

    Fix descriptions of PoseErrorCostFunction

--- src/colmap/estimators/cost_functions.h
+++ src/colmap/estimators/cost_functions.h
@@ -332,15 +332,15 @@ class RigReprojErrorCostFunction {
 // [x, y, z, w].
 
 // The estimated measurement is:
-//      t_ab = [ p_ab ]  = [ R(q_a)^T * (p_b - p_a) ]
-//             [ q_ab ]    [ q_a^{-1] * q_b         ]
+//      t_ab = [ t_ab ]  = [ q_a^{-1} * (t_b - t_a) ]
+//             [ q_ab ]    [ q_a^{-1} * q_b         ]
 //
 // where ^{-1} denotes the inverse and R(q) is the rotation matrix for the
 // quaternion. Now we can compute an error metric between the estimated and
 // measurement transformation. For the orientation error, we will use the
 // standard multiplicative error resulting in:
 //
-//   error = [ p_ab - \hat{p}_ab                 ]
+//   error = [ t_ab - \hat{t}_ab                 ]
 //           [ 2.0 * Vec(q_ab * \hat{q}_ab^{-1}) ]
 //
 // where Vec(*) returns the vector (imaginary) part of the quaternion. Since
@@ -355,24 +355,24 @@ class RigReprojErrorCostFunction {
 class PoseErrorCostFunction {
  public:
   PoseErrorCostFunction(const Eigen::Quaterniond& q_ab_measured,
-                        const Eigen::Vector3d& p_ab_measured,
+                        const Eigen::Vector3d& t_ab_measured,
                         const double weight_rotation,
-                        const double weight_position)
+                        const double weight_translation)
       : q_ab_measured_(q_ab_measured),
-        p_ab_measured_(p_ab_measured),
+        t_ab_measured_(t_ab_measured),
         weight_rotation_(2.0 * weight_rotation),
-        weight_position_(weight_position) {}
+        weight_translation_(weight_translation) {}
 
   static ceres::CostFunction* Create(const Eigen::Quaterniond& q_ab_measured,
-                                     const Eigen::Vector3d& p_ab_measured,
+                                     const Eigen::Vector3d& t_ab_measured,
                                      const double weight_rotation = 1.0,
-                                     const double weight_position = 1.0) {
+                                     const double weight_translation = 1.0) {
     return (
         new ceres::AutoDiffCostFunction<PoseErrorCostFunction, 6, 4, 3, 4, 3>(
             new PoseErrorCostFunction(q_ab_measured,
-                                      p_ab_measured,
+                                      t_ab_measured,
                                       weight_rotation,
-                                      weight_position)));
+                                      weight_translation)));
   }
 
   template <typename T>
@@ -383,42 +383,40 @@ class PoseErrorCostFunction {
                   T* residuals) const {
     Eigen::Map<const Eigen::Matrix<T, 3, 1>> t_a(cam1_from_world_translation);
     Eigen::Map<const Eigen::Quaternion<T>> q_a(cam1_from_world_rotation);
-    Eigen::Matrix<T, 3, 1> p_a = q_a * (-t_a);
 
     Eigen::Map<const Eigen::Matrix<T, 3, 1>> t_b(cam2_from_world_translation);
     Eigen::Map<const Eigen::Quaternion<T>> q_b(cam2_from_world_rotation);
-    Eigen::Matrix<T, 3, 1> p_b = q_b * (-t_b);
 
     // Compute the relative transformation between the two frames.
     Eigen::Quaternion<T> q_a_inverse = q_a.conjugate();
     Eigen::Quaternion<T> q_ab_estimated = q_a_inverse * q_b;
 
     // Represent the displacement between the two frames in the A frame.
-    Eigen::Matrix<T, 3, 1> p_ab_estimated = q_a_inverse * (p_b - p_a);
+    Eigen::Matrix<T, 3, 1> t_ab_estimated = q_a_inverse * (t_b - t_a);
 
     // Compute the error between the two orientation estimates.
     Eigen::Quaternion<T> delta_q =
         q_ab_measured_.template cast<T>() * q_ab_estimated.conjugate();
 
     // Compute the residuals.
-    // [ position         ]   [ delta_p          ]
+    // [ translation      ]   [ delta_t          ]
     // [ orientation (3x1)] = [ 2 * delta_q(0:2) ]
     Eigen::Map<Eigen::Matrix<T, 6, 1>> residuals_ref(residuals);
     residuals_ref.template block<3, 1>(3, 0) =
         T(weight_rotation_) * delta_q.vec();
 
     residuals_ref.template block<3, 1>(0, 0) =
-        T(weight_position_) *
-        (p_ab_estimated - p_ab_measured_.template cast<T>());
+        T(weight_translation_) *
+        (t_ab_estimated - t_ab_measured_.template cast<T>());
 
     return true;
   }
 
  private:
   Eigen::Quaterniond q_ab_measured_;
-  Eigen::Vector3d p_ab_measured_;
+  Eigen::Vector3d t_ab_measured_;
   double weight_rotation_;
-  double weight_position_;
+  double weight_translation_;
 };
 
 class PoseCenterErrorCostFunction {
